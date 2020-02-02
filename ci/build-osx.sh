d709a76fc2358dea1332d8e4107b82f67c878b89
Eddy Gharbi <eddygharbiema@gmail.com>
2020-02-02 15:11:43 -0800

    Revert "Use build directory env location"
    This reverts commit 1b9b4df3c22e5d156bb57b98538ba4cb08657a96.

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -6,14 +6,19 @@ export DEPS="libuiohook-osx-1.1-sl.0"
 wget --quiet --retry-connrefused --waitretry=1 https://obs-studio-deployment.s3-us-west-2.amazonaws.com/libuiohook-osx-1.1-sl.0.tar.gz
 tar -xf ./${DEPS}.tar.gz -C ./deps
 
+mkdir build
+cd build
+
 # Configure
-cmake ${BUILDDIRECTORY} \
--DUIOHOOKDIR=${PWD}/./deps/${DEPS} \
+cmake .. \
+-DUIOHOOKDIR=${PWD}/../deps/${DEPS} \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 -DCMAKE_INSTALL_PREFIX=${FULLDISTRIBUTEPATH}/node-libuiohook
 -DNODEJS_NAME=${RUNTIMENAME} \
 -DNODEJS_URL=${RUNTIMEURL} \
 -DNODEJS_VERSION=${RUNTIMEVERSION}
 
+cd ..
+
 # Build
-cmake --build ${BUILDDIRECTORY} --target install --config RelWithDebInfo
+cmake --build build --target install --config RelWithDebInfo
