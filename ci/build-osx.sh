1b9b4df3c22e5d156bb57b98538ba4cb08657a96
Eddy Gharbi <eddygharbiema@gmail.com>
2020-02-02 15:04:58 -0800

    Use build directory env location

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -6,19 +6,14 @@ export DEPS="libuiohook-osx-1.1-sl.0"
 wget --quiet --retry-connrefused --waitretry=1 https://obs-studio-deployment.s3-us-west-2.amazonaws.com/libuiohook-osx-1.1-sl.0.tar.gz
 tar -xf ./${DEPS}.tar.gz -C ./deps
 
-mkdir build
-cd build
-
 # Configure
-cmake .. \
--DUIOHOOKDIR=${PWD}/../deps/${DEPS} \
+cmake ${BUILDDIRECTORY} \
+-DUIOHOOKDIR=${PWD}/./deps/${DEPS} \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 -DCMAKE_INSTALL_PREFIX=${FULLDISTRIBUTEPATH}/node-libuiohook
 -DNODEJS_NAME=${RUNTIMENAME} \
 -DNODEJS_URL=${RUNTIMEURL} \
 -DNODEJS_VERSION=${RUNTIMEVERSION}
 
-cd ..
-
 # Build
-cmake --build build --target install --config RelWithDebInfo
+cmake --build ${BUILDDIRECTORY} --target install --config RelWithDebInfo
