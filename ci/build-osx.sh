614e2587a33c083c0d3e696eb0e3942436d6debf
Vladimir Sumarov <summeroff@gmail.com>
2022-06-17 14:57:21 -0700

    sentry upload command

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -34,6 +34,6 @@ sudo install_name_tool -change \
 #Upload debug files
 curl -sL https://sentry.io/get-cli/ | bash
 dsymutil $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_libuiohook.node
-sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-client $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_libuiohook.node.dSYM/Contents/Resources/DWARF/node_libuiohook.node
+sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-client $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_libuiohook.node.dSYM/Contents/Resources/DWARF/node_libuiohook.node --log-level=debug
 
 rm -rf ./build/deps
